220b4b970274ca63ed6c9e83c2e57f3b09dc46b3
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-02-28 14:33:11 -0300

    feat: add NotificationResultPublisher to publish result messages and update the schedules in notification_scheduler

--- src/main/java/com/yuriolivs/notification_service/notification/messaging/producer/NotificationResultPublisher.java
+++ src/main/java/com/yuriolivs/notification_service/notification/messaging/producer/NotificationResultPublisher.java
@@ -1,4 +1,24 @@
 package com.yuriolivs.notification_service.notification.messaging.producer;
 
+import com.yuriolivs.notification_service.config.RabbitMqConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.management.remote.NotificationResult;
+
+@Component
+@RequiredArgsConstructor
 public class NotificationResultPublisher {
+    @Autowired
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(NotificationResult result) {
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.EXCHANGE,
+                RabbitMqConfig.RESULT_ROUTING_KEY,
+                result
+        );
+    }
 }
